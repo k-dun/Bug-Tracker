1495bce75ad2f86ab85ae0204270780af4ce5685
Konrad <kdun.work@gmail.com>
2023-02-07 20:53:06 +0000

    Created an initial outline of methods required.

---
+++ lib/bug-tracker.rb
@@ -0,0 +1,49 @@
+require 'CSV'
+
+@issues = []
+
+# USER INTERFACE METHODS
+
+#
+def interactive_menu
+
+end
+
+# CREATE, READ, UPDATE, DELETE METHODS
+
+#
+def create_issue
+
+end
+
+#
+def read_all_issues
+
+end
+
+#
+def update_issue
+
+end
+
+#
+def delete_issue
+
+end
+
+# OTHER METHODS
+
+#
+def filter_issues
+
+end
+
+#
+def generate_report
+
+end
+
+#
+def print_report
+
+end
