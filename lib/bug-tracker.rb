f4dcfc6da4e7e34326337a13a86cb1966ce1278e
Konrad <kdun.work@gmail.com>
2023-02-08 09:35:32 +0000

    Added comments for each method to describe its purpose.

--- lib/bug-tracker.rb
+++ lib/bug-tracker.rb
@@ -4,46 +4,46 @@ require 'CSV'
 
 # USER INTERFACE METHODS
 
-#
+# 
 def interactive_menu
 
 end
 
 # CREATE, READ, UPDATE, DELETE METHODS
 
-#
+# Create an issue (bug). Includes a title, description, status.
 def create_issue
 
 end
 
-#
-def read_all_issues
+# Print all issues.
+def print_all_issues
 
 end
 
-#
-def update_issue
+# Update bug's status.
+def update_status
 
 end
 
-#
+# Delete bug record.
 def delete_issue
 
 end
 
 # OTHER METHODS
 
-#
+# Filter bugs by status.
 def filter_issues
 
 end
 
-#
+# Generate report to a file.
 def generate_report
 
 end
 
-#
+# Print report from a file.
 def print_report
 
 end
