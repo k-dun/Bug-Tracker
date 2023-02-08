7dd861358e12c4f3e78110b885a84a8f51039c30
Konrad <kdun.work@gmail.com>
2023-02-08 12:34:34 +0000

    Added header and a method to print timeline of changes to the Bug Tracker.

--- lib/bug_tracker.rb
+++ lib/bug_tracker.rb
@@ -1,17 +1,26 @@
+#!/usr/bin/env ruby
+
 require 'CSV'
 
 @issues = []
+@bug_tracker_version = "1.0"
 
 # USER INTERFACE METHODS
 
 # Main interactive menu to help navigate and interact with the bug tracker.
 def interactive_menu
-  puts "\nWelcome to Bug Tracker 1.0! (author: kdun)"
-
+  print_header()
   loop do 
-    print_menu
+    print_menu()
     menu_options(STDIN.gets.chomp)
   end
+end
+
+# Print application's header.
+def print_header
+  puts "-------------"
+  puts "Welcome to Bug Tracker #{@bug_tracker_version}! (author: kdun)"
+  puts "-------------"
 end
 
 # Print menu options.
@@ -23,7 +32,8 @@ def print_menu
   puts "5. Delete an issue."
   puts "6. Generate a report."
   puts "7. Print recent report."
-  puts "8. Exit."
+  puts "8. View commits and updates of Bug Tracker 1.0."
+  puts "9. Exit."
 end
 
 # Select menu option.
@@ -44,7 +54,9 @@ def menu_options(menu_choice)
   when "7"
     print_report()
   when "8"
-    puts "Quitting Bug Tracker 1.0 ..."
+    bug_tracker_updates()
+  when "9"
+    puts "Quitting Bug Tracker #{@bug_tracker_version} ..."
     exit
   else
     puts "Wrong input. Try again!"
@@ -63,19 +75,19 @@ def print_all_issues
 
 end
 
-# Update bug's status.
+# Update issue status.
 def update_status
 
 end
 
-# Delete bug record.
+# Delete issue record.
 def delete_issue
 
 end
 
 # OTHER METHODS
 
-# Filter bugs by status.
+# Filter issues by status.
 def filter_issues
 
 end
@@ -89,5 +101,12 @@ end
 def print_report
 
 end
+
+# Print history of updates to the Bug Tracker 
+def bug_tracker_updates
+
+end
+
+# MAIN 
 
 interactive_menu
